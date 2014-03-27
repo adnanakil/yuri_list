0e72b35447cec199c0427d1998f48ce4de1f20a1
Adnan Akil <adnanakil@gmail.com>
2014-03-26 22:00:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YuriList::Application.config.secret_key_base = '0658ede73ea4ae6e649d08cf9f65aa6f64e39772774a7991e16e3a5f661d68b476ab84725c23cd726e566cef4473a85f9fd3d81cc2edbe471f37cc6ba21ca785'
